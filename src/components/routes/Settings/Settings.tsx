2368b9ea5aa261b9a7da2539a73ab910d0a67bb1
Chuckie <larssalling98@gmail.com>
2020-02-17 21:11:49 +0100

    Store settings sections in variable and map them in render

--- src/components/routes/Settings/Settings.tsx
+++ src/components/routes/Settings/Settings.tsx
@@ -18,21 +18,31 @@ const Settings = () => {
       title: "Profile Settings",
       subtitle: "Visual preferences",
       url: "/#"
+    },
+    {
+      title: "Login History",
+      subtitle: "View previous login attempts",
+      url: "/#",
+      break: true
     }
   ];
   return (
     <MainLayout>
-      <div className="border-t flex-wrap border-gray-400 flex mb-2">
-        {sections.map(section => (
+      <div className="flex-wrap flex mb-2 border-gray-400">
+        {sections.map((section, idx) => (
           <Link
+            key={idx}
             to={section.url}
             href="/accountsettings.html"
-            className="py-2 px-4 bg-white w-full border-b border-gray-300"
+            className={`py-2 px-4 bg-white w-full border-b border-gray-400 dark:bg-gray-800 dark:border-gray-700 ${section.break &&
+              "mt-2 border-t"}`}
           >
             <div className="flex justify-between">
               <div className="flex flex-col leading-tight">
-                <h4 className="text-gray-800">{section.title}</h4>
-                <span className="text-gray-500 text-xs">
+                <h4 className="text-gray-800 dark:text-gray-500">
+                  {section.title}
+                </h4>
+                <span className="text-gray-500 text-xs dark:text-gray-600">
                   {section.subtitle}
                 </span>
               </div>
@@ -40,19 +50,6 @@ const Settings = () => {
             </div>
           </Link>
         ))}
-      </div>
-      <div className="border-t flex-wrap border-b border-gray-400 flex mb-2">
-        <a href="/accountsettings.html" className="py-2 px-4 bg-white w-full">
-          <div className="flex justify-between">
-            <div className="flex flex-col leading-tight">
-              <h4 className="text-gray-800">Login History</h4>
-              <span className="text-gray-500 text-xs">
-                View previous login-attempts...
-              </span>
-            </div>
-            <i className="fas fa-caret-right text-sm self-center text-gray-500" />
-          </div>
-        </a>
       </div>
       <div className="border-t flex-wrap border-b border-gray-400 flex mb-2">
         <a href="/accountsettings.html" className="py-2 px-4 bg-white w-full">
