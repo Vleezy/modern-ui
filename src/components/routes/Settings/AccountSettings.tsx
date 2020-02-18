a4a0e780c10fcfc5f5f7eb9ce651d927c881b596
Chuckie <larssalling98@gmail.com>
2020-02-18 21:40:09 +0100

    add account settings route

---
+++ src/components/routes/Settings/AccountSettings.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MainLayout from "components/layout/MainLayout";
+
+const AccountSettings = () => {
+  return (
+    <MainLayout>
+      <div className="p-2 w-full">
+        <h4 className="text-xs text-gray-500 font-semibold">Account setting</h4>
+        <div className="w-full"></div>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default AccountSettings;
