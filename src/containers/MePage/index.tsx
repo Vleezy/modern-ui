51dbbfcdb5a11fe0a97939f11680c3def48577bc
Chuckie <chuckmail98@gmail.com>
2020-03-10 15:23:34 +0100

    Remove unused Modal component

--- src/containers/MePage/index.tsx
+++ src/containers/MePage/index.tsx
@@ -35,53 +35,46 @@ const Me = () => {
   const [badgesModalVisible, setBadgesModalVisible] = useState(true);
 
   return (
-    <>
-      {badgesModalVisible && (
-        <DefaultModal
-          closeModal={() => setBadgesModalVisible(false)}
-        ></DefaultModal>
-      )}
-      <MainLayout isHomepage={true}>
-        <div className="hidden lg:flex">
-          <div className="bg-background rounded w-2/3 flex justify-between bg-white border border-gray-400 p-1 flex flex-col mr-2 self-start dark:bg-gray-800 dark:border-gray-700">
+    <MainLayout isHomepage={true}>
+      <div className="hidden lg:flex">
+        <div className="bg-background rounded w-2/3 flex justify-between bg-white border border-gray-400 p-1 flex flex-col mr-2 self-start dark:bg-gray-800 dark:border-gray-700">
+          <div
+            className="bg-center w-full h-32 rounded-t flex justify-end bg-surface"
+            style={{
+              backgroundImage: `url(/assets/images/view_ca_wide.png)`
+            }}
+          >
+            <div className="h-full flex flex-col justify-center">
+              <button className="self-center flex px-4 py-2 bg-green-600 mr-2 border border-green-500 rounded">
+                Enter hotel _>
+              </button>
+            </div>
+          </div>
+          <div className="relative h-10 rounded-b-sm flex">
             <div
-              className="bg-center w-full h-32 rounded-t flex justify-end bg-surface"
+              className="absolute bg-no-repeat bg-bottom -mt-24 ml-2"
               style={{
-                backgroundImage: `url(/assets/images/view_ca_wide.png)`
+                backgroundImage: `url(/assets/images/podium.png)`,
+                padding: `0 25px 38px 22px`
               }}
             >
-              <div className="h-full flex flex-col justify-center">
-                <button className="self-center flex px-4 py-2 bg-green-600 mr-2 border border-green-500 rounded">
-                  Enter hotel _>
-                </button>
-              </div>
+              <img
+                alt="Habbo Figure"
+                src={`https://www.habbo.nl/habbo-imaging/avatarimage?figure=${process.env.REACT_APP_HABBO_FIGURE}`}
+              />
             </div>
-            <div className="relative h-10 rounded-b-sm flex">
-              <div
-                className="absolute bg-no-repeat bg-bottom -mt-24 ml-2"
-                style={{
-                  backgroundImage: `url(/assets/images/podium.png)`,
-                  padding: `0 25px 38px 22px`
-                }}
-              >
-                <img
-                  alt="Habbo Figure"
-                  src={`https://www.habbo.nl/habbo-imaging/avatarimage?figure=${process.env.REACT_APP_HABBO_FIGURE}`}
-                />
-              </div>
-              <div className="ml-32"></div>
-              <div className="bg-blue-400">Test</div>
-            </div>
-          </div>
-          <div className="bg-gray-100 border border-gray-400 rounded w-1/3 p-1 self-start dark:bg-gray-800 dark:border-gray-700">
-            <NewsPreviewContainer />
+            <div className="ml-32"></div>
+            <div className="bg-blue-400">Test</div>
           </div>
         </div>
-        <div className="block lg:hidden">
-          {renderTab(currentHomeTab || "NEWS")}
+        <div className="bg-gray-100 border border-gray-400 rounded w-1/3 p-1 self-start dark:bg-gray-800 dark:border-gray-700">
+          <NewsPreviewContainer />
         </div>
-      </MainLayout>
-    </>
+      </div>
+      <div className="block lg:hidden">
+        {renderTab(currentHomeTab || "NEWS")}
+      </div>
+    </MainLayout>
   );
 };
 
