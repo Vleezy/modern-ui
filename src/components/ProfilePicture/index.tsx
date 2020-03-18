f54f2ca7c382527e81d61bec6fbd0f49b200d88e
Chuckie <chuckmail98@gmail.com>
2020-03-18 09:12:08 +0100

    Centralize profile picture component

--- src/components/ProfilePicture/index.tsx
+++ src/components/ProfilePicture/index.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import { IUser } from 'models/user/IUser';
+import React from 'react';
 
 interface ProfilePictureProps {
   figure?: string;
+  user?: IUser;
   styles?: string;
   online?: boolean;
+  size?: "sm" | "md" | "lg";
 }
 
 const ProfilePicture = (props: ProfilePictureProps) => {
-  const { figure, styles, online } = props;
+  const { figure, styles, online, user, size = "md" } = props;
 
-  const habboFigure = {
+  const habboFigureMd = {
     backgroundImage: `url("https://www.habbo.nl/habbo-imaging/avatarimage?figure=${figure}")`,
     backgroundPositionY: "-14px",
     backgroundPositionX: "-9px"
+  };
+
+  const habboFigureLg = {
+    backgroundImage: `url("https://www.habbo.nl/habbo-imaging/avatarimage?figure=${figure}&size=l")`,
+    backgroundPositionY: "-28px",
+    backgroundPositionX: "-14px"
   };
 
   const habboFigureBanned = {
@@ -34,17 +43,53 @@ const ProfilePicture = (props: ProfilePictureProps) => {
   if (!figure)
     return <div className={`rounded w-12 h-12 flex-shrink-0 ${styles}`}></div>;
 
-  return (
-    <div
-      id="profile-pic"
-      className={`${styles &&
-        styles} rounded shadow-inner w-12 h-12 flex-shrink-0 relative`}
-      style={isBanned ? habboFigureBanned : habboFigure}
-    >
-      {/* Render online indicator if set in props */}
-      {typeof online !== "undefined" && onlineIndicator}
-    </div>
-  );
+  switch (size) {
+    case "sm":
+      return (
+        <div
+          className={`h-8 w-8 bg-center flex-shrink-0 bg-no-repeat rounded bg-white-500 ${styles}`}
+          style={habboFigureMd}
+        >
+          {/* Render online indicator if set in props */}
+          {typeof user?.online !== "undefined" && onlineIndicator}
+        </div>
+      );
+
+    case "md":
+      return (
+        <div
+          id="profile-pic"
+          className={`${styles &&
+            styles} rounded shadow-inner w-12 h-12 flex-shrink-0 relative`}
+          style={isBanned ? habboFigureBanned : habboFigureMd}
+        >
+          {/* Render online indicator if set in props */}
+          {typeof online !== "undefined" && onlineIndicator}
+        </div>
+      );
+
+    case "lg":
+      return (
+        <div
+          id="profile-pic"
+          className={`${styles &&
+            styles} rounded shadow-inner w-24 h-24 flex-shrink-0 relative`}
+          style={
+            isBanned
+              ? habboFigureBanned
+              : size === "lg"
+              ? habboFigureLg
+              : habboFigureMd
+          }
+        >
+          {/* Render online indicator if set in props */}
+          {typeof online !== "undefined" && onlineIndicator}
+        </div>
+      );
+
+    default:
+      return <div>Invalid size!</div>;
+  }
 };
 
 export default ProfilePicture;
--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -13,33 +13,25 @@ const Profile = () => {
 
   return (
     <MainLayout headerTransparent>
-      <div className="flex flex-col w-full -mt-12">
+      <div className="flex flex-col w-full -mt-12 relative">
         <div
           className="flex w-full h-40 bg-center border-b-2 border-white"
           style={{
             backgroundImage: `url('/assets/images/profile_backgrounds/bubble.gif')`
           }}
-        />
-        <div className="-mt-6 flex w-2/3 mx-auto">
-          <div className="mr-2">
-            <ProfilePicture
-              styles="bg-white"
-              figure={process.env.REACT_APP_HABBO_FIGURE}
-            />
-          </div>
-          <div className="bg-white rounded flex p-1 flex-grow justify-between">
-            <div>
-              <button></button>
-              <button></button>
+        >
+          <div className="flex mx-2 absolute top-0">
+            <div className="mr-2">
+              <ProfilePicture
+                size="lg"
+                styles="bg-white"
+                figure={process.env.REACT_APP_HABBO_FIGURE}
+              />
             </div>
-            <button className="bg-blue-500 text-white text-sm p-1 rounded">
-              <i className="fas fa-plus text-xs mr-1" />
-              Add friend
-            </button>
           </div>
         </div>
-
-        <div className="p-2">
+        <div className="px-2">
+          <div className="h-10 w-full bg-blue-200"></div>
           <Button>Test</Button>
         </div>
       </div>
